4963e453ce705b90b38f39c7f3aaa14b153b807b
Sock <87631534+platonicsock@users.noreply.github.com>
2026-06-11 19:44:44 +0300

    Made Anneal the playground default upon opening

--- ui/frontend/reducers/configuration.ts
+++ ui/frontend/reducers/configuration.ts
@@ -13,6 +13,7 @@ import {
   PairCharacters,
   PrimaryAction,
   PrimaryActionAuto,
+  PrimaryActionCore,
   ProcessAssembly,
   Theme,
 } from '../types';
@@ -55,7 +56,7 @@ const initialState: State = {
   assemblyFlavor: AssemblyFlavor.Att,
   demangleAssembly: DemangleAssembly.Demangle,
   processAssembly: ProcessAssembly.Filter,
-  primaryAction: PrimaryActionAuto.Auto,
+  primaryAction: PrimaryActionCore.Anneal,
   channel: Channel.Stable,
   mode: Mode.Debug,
   edition: Edition.Rust2024,
